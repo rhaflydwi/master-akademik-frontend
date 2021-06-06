a9fd6fd21a12f4bc406ae408d3b9c6051179af87
rhaflydwi <rhaflydwi01@gmail.com>
2021-06-07 00:51:58 +0800

    Update Layout Sidebar

---
+++ components/layouts/SidebarGuru.vue
@@ -0,0 +1,57 @@
+<template>
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fas fa-laugh-wink"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">AKADEMIKTA <sup></sup></div>
+        </a>
+        <hr class="sidebar-divider my-0">
+        <li class="nav-item active">
+            <a class="nav-link" href="/dashboardguru">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <hr class="sidebar-divider">
+        <div class="sidebar-heading">
+    Master Data
+</div>
+                <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                <i class="fas fa-fw fa-clock"></i>
+                <span>Data Presensi</span>
+            </a>
+            <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">List Presensi :</h6>
+                    <nuxt-link class="collapse-item" :to="{name: 'PresensiGuru'}">Presensi Guru</nuxt-link>
+                    <nuxt-link class="collapse-item" :to="{name: 'PresensiSiswa'}">Presensi Siswa</nuxt-link>
+                </div>
+            </div>
+        </li>
+                <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsefive" aria-expanded="true" aria-controls="collapsefive">
+                <i class="fas fa-fw fa-calendar"></i>
+                <span>Data Jadwal</span>
+            </a>
+            <div id="collapsefive" class="collapse" aria-labelledby="headingfive" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Data Jadwal :</h6>
+                    <nuxt-link class="collapse-item" :to="{name: 'JadwalSiswaGuru'}"> Jadwal Siswa All</nuxt-link>
+                    <nuxt-link class="collapse-item" :to="{name: 'JadwalSiswaGuruVII'}"> Jadwal Siswa VII</nuxt-link>
+                    <nuxt-link class="collapse-item" :to="{name: 'JadwalSiswaGuruVIII'}"> Jadwal Siswa VIII</nuxt-link>
+                    <nuxt-link class="collapse-item" :to="{name: 'JadwalSiswaGuruIX'}"> Jadwal Siswa IX</nuxt-link>
+                </div>
+            </div>
+        </li>
+              <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+    </ul>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ components/layouts/SidebarLaboratorium.vue
@@ -0,0 +1,41 @@
+<template>
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fas fa-laugh-wink"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">AKADEMIKTA <sup></sup></div>
+        </a>
+        <hr class="sidebar-divider my-0">
+        <li class="nav-item active">
+            <a class="nav-link" href="/dashboardadmin">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <hr class="sidebar-divider">
+        <div class="sidebar-heading">
+    Master Data
+</div>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                <i class="fas fa-fw fa-flask"></i>
+                <span>Data Laboratorium</span>
+            </a>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Data Laboratorium :</h6>
+                    <nuxt-link class="collapse-item" :to="{name: 'LaboratoriumAlat'}">Alat Laboratorium</nuxt-link>
+                </div>
+            </div>
+        </li>
+              <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+    </ul>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
